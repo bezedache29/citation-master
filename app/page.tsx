1cbc1b3de1b423bf9660b12974a998ce1d4af1f0
Bezedache <bezedache29@gmail.com>
2025-06-23 14:42:08 +0200

    init project

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/src/components/ui/button";
+import { Input } from "@/src/components/ui/input";
+
+export default function Home() {
+  return (
+    <div>
+      <Button>Hello</Button>
+      <Input />
+    </div>
+  );
+}
